f5cb0e0bc53d8c4275a54b6b3fb980043a670bf4
Alex <alexcoh19@outlook.com>
2025-09-30 10:16:03 +0200

    Added poperties and methods Person class

--- Hotel2/Manpower/Person.cs
+++ Hotel2/Manpower/Person.cs
@@ -8,5 +8,19 @@ namespace Hotel2.Manpower
 {
     internal class Person
     {
+        protected string Name { get; set; }
+        protected int Age { get; set; }
+        protected string EmployeeId { get; set; }
+        protected DateTime StartDate { get; set; }
+        protected decimal Salary { get; set; }
+
+        public void PrintInfo()
+        {
+
+        }
+        public void Introduce()
+        {
+
+        }
     }
 }
